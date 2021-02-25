3ba9ea13f11ee37e75e1e68dfe0cb7c80aeb4a5c
ni_n_ja <rampage4extrm@gmail.com>
2021-02-26 08:51:59 +0900

    a11y対応

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,30 @@ var browserSync = require('browser-sync');
 var svgmin = require('gulp-svgmin');
 var del = require('del');
 
-gulp.task('reviveSVG', function () {
+gulp.task('reviveSVG', (done) => {
     del(['svg_minified/**/*.svg'], function (err, deleted) {
         console.log('deleted: ' + deleted.join(','));
     });
     gulp.src('svg/**/*.svg')
         .pipe(svgmin())
         .pipe(gulp.dest('./svg_minified'));
+    done();
 });
 
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', (done) => {
     browserSync({
         files: ['service-worker.js', 'js/**/*.js', '**/*.html', 'css/**/*.css'],
         server: {
             baseDir: "./"
         },
         port: 9001,
-        https: true,
+        // https: true,
         open: false
     });
+    done();
 });
 
-gulp.task('default', ['reviveSVG', 'browser-sync'], function () {
+gulp.task('default', gulp.series('reviveSVG', 'browser-sync'), (done) => {
     gulp.watch(['svg/**/*.svg'], ['reviveSVG', 'browser-sync']);
+    done();
 });
